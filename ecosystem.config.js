321e144ccb5c584604360517f48f85bca9b17b9b
Tudor Constantin <tudorgalt@gmail.com>
2022-05-05 10:51:33 +0300

    Update the config to point to the latest deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
     max_memory_restart : "1000M",
     log_date_format : "YYYY-MM-DD HH:mm Z",
     env: {
-      ORDER_BOOK_ADDRESS: "0xF683eED9590E2f90fe991E9e5A736f8BEDEa84Cd",
+      ORDER_BOOK_ADDRESS: "0x63d4076105798196acC55D31dCa57E518bb64Bf8",
       IDX_ADDR_START: 0,
       NUM_ADDRESSES: 3,
       PERP_NAME: 'BTCUSD',
@@ -17,7 +17,7 @@ module.exports = {
     max_memory_restart : "1000M",
     log_date_format : "YYYY-MM-DD HH:mm Z",
     env: {
-      ORDER_BOOK_ADDRESS: "0xA9a91c803a994332c1020D2DACFEBbfC53D65533",
+      ORDER_BOOK_ADDRESS: "0x152C5ae9F8E6Adb91a97C38cD4F83ceE997D6FC2",
       IDX_ADDR_START: 3,
       NUM_ADDRESSES: 3,
       PERP_NAME: 'BNBUSD',
